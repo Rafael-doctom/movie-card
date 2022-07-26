6a1311eb81c6f9ac5651b5ceea2af00346292448
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-07-26 13:56:09 -0300

    feat: data changed and configured  find the movies

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,17 +8,29 @@ const config = {
     image_base_url: 'https://image.tmdb.org/t/p/w500',
     imageError: 'https://www.diamed.med.br/wp-content/uploads/diamed-banner-erro301.jpg',
     language: 'language=pt-BR',
-    example: `https://api.themoviedb.org/3/movie/453395?api_key=103186a9f4ef77e1f666cdd93a1fa70a`,
+    example: `https://api.themoviedb.org/3/movie/293660?api_key=103186a9f4ef77e1f666cdd93a1fa70a&language=pt-BR`,
     exampleRandom: `https://api.themoviedb.org/3/movie/${random}?api_key=103186a9f4ef77e1f666cdd93a1fa70a`,
     token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDMxODZhOWY0ZWY3N2UxZjY2NmNkZDkzYTFmYTcwYSIsInN1YiI6IjYyZGVmMzliYmJjYWUwMDA1MDEzYTg4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oT9bykoKEOw31Mrkbnjitrc3Bxthf-E6b8jRnjPcEyE'
 };
 
 const getBannerMovie = () => {
+
+    const paragraph = document.getElementById("paragraph");
+    const genres = document.getElementById("genres");
+    const release_date = document.getElementById("release_date");
+    const time = document.getElementById("time");
+
     fetch(config.example)
         .then(response => (response.json()))
-        .then(json => banner.src = `https://image.tmdb.org/t/p/w500${json.backdrop_path}`)
-        .catch(() => console.log('Que pena! Não encontramos nenhum filme.'));
-};
+        // .then(json =>  document.getElementById("hero-image").style.backgroundImage = `url("https://image.tmdb.org/t/p/w500${json.backdrop_path}")`)
+        .then(json => {
+            paragraph.innerText = json.original_title
+            genres.innerText = `${json.genres[0].name} ${json.genres[2].name}`
+            release_date.innerText = json.release_date
+            time.innerText = `${json.runtime} minutos`
+        })
+        .catch(() => console.log('Que pena! Não encontramos nenhum filme.'))
 
+}
 
 getBannerMovie();
