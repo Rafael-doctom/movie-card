2e079110563c0b9f9e5ba82c3e47fb19a3ebd867
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-07-26 12:13:11 -0300

    feat: data with basic config added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,24 @@
+const min = 555;
+const max = 700;
+const random = Math.floor(max * Math.random() + min * 2 + max);
+
 const config = {
     api_key: '103186a9f4ef77e1f666cdd93a1fa70a',
     api_base_url: 'https://api.themoviedb.org/3/',
     image_base_url: 'https://image.tmdb.org/t/p/w500',
+    imageError: 'https://www.diamed.med.br/wp-content/uploads/diamed-banner-erro301.jpg',
     language: 'language=pt-BR',
-    example: 'https://api.themoviedb.org/3/movie/550?api_key=103186a9f4ef77e1f666cdd93a1fa70a', 
+    example: `https://api.themoviedb.org/3/movie/453395?api_key=103186a9f4ef77e1f666cdd93a1fa70a`,
+    exampleRandom: `https://api.themoviedb.org/3/movie/${random}?api_key=103186a9f4ef77e1f666cdd93a1fa70a`,
     token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDMxODZhOWY0ZWY3N2UxZjY2NmNkZDkzYTFmYTcwYSIsInN1YiI6IjYyZGVmMzliYmJjYWUwMDA1MDEzYTg4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oT9bykoKEOw31Mrkbnjitrc3Bxthf-E6b8jRnjPcEyE'
-}
+};
+
+const getBannerMovie = () => {
+    fetch(config.example)
+        .then(response => (response.json()))
+        .then(json => banner.src = `https://image.tmdb.org/t/p/w500${json.backdrop_path}`)
+        .catch(() => console.log('Que pena! Não encontramos nenhum filme.'));
+};
+
+
+getBannerMovie();
