98a6ba732e348d9945acc0ad66edfcda0d6af4c2
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-07-26 11:10:45 -0300

    feat: initial configuration api data

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+const config = {
+    api_key: '103186a9f4ef77e1f666cdd93a1fa70a',
+    api_base_url: 'https://api.themoviedb.org/3/',
+    image_base_url: 'https://image.tmdb.org/t/p/w500',
+    language: 'language=pt-BR',
+    example: 'https://api.themoviedb.org/3/movie/550?api_key=103186a9f4ef77e1f666cdd93a1fa70a', 
+    token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDMxODZhOWY0ZWY3N2UxZjY2NmNkZDkzYTFmYTcwYSIsInN1YiI6IjYyZGVmMzliYmJjYWUwMDA1MDEzYTg4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oT9bykoKEOw31Mrkbnjitrc3Bxthf-E6b8jRnjPcEyE'
+}
