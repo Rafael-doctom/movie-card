e2619fd91930c5514587c28b58c25ffd24843c0c
Guilherme <guilherme.salviano@hotmail.com>
2022-07-30 16:50:50 -0300

    component is done

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,27 @@
 
 
 // guilherme dará continuidade aqui
-import {getFolder} from "./functions.js"
 
+//Component inserted inside the section "movies":
+
+const component = (id, src, year, name, likes) =>{
+    const content = ` 
+    <div class="movie__box" data-id="${id}">
+    <img src="${src}" alt="" class="movies__img" 
+    <div class="movie__details">
+        <p class="details__name">${name}</p>
+        <p class="details__year">Ano de lançamento: ${year}</p>
+        <p class="details__likes">Avaliação: ${likes}</p>
+    </div>
+    </div>`
+
+  const element = document.createElement('div')
+  element.classList.add('movie__container')
+  element.innerHTML = content
+  return element
+}
+
+//Here we make a request to tmdb api
 function request (){
     return fetch('https://api.themoviedb.org/3/discover/movie?api_key=103186a9f4ef77e1f666cdd93a1fa70a&?page=1&sort_by=popularity.desc', {
         method: 'GET',
@@ -15,21 +34,23 @@ function request (){
     })
     
 }
-
+//Here we show the folders
 (async()=>{
     const api = await request()
     const apiResponse = await api.json()
     console.log(apiResponse)
-    for(let a = 1; a < 7; a++){
-        const elements = getFolder(a)
-        const t = apiResponse.results[a].vote_average
-        elements.movieName.innerHTML = apiResponse.results[a].original_title
-        elements.movieYear.innerHTML = apiResponse.results[a].release_date.slice(0,4)
-        elements.movieLikes.innerHTML = apiResponse.results[a].vote_average
-        elements.movieName.innerHTML = apiResponse.results[a].original_title
-        elements.movieImg.src = `https://image.tmdb.org/t/p/w500${apiResponse.results[a].poster_path}`
-        elements.movieName.dataset.id = apiResponse.results[a].id
+    //This const get the section where we append each movie folder
+    const movieBanner = document.querySelector('[data-movies]')
+    //This looping add a new movie folder based on the api response. We'll have as many folders as loopings we do
+    for(let a = 1; a < 10; a++){
+        const element = component(
+            apiResponse.results[a].id,
+            `https://image.tmdb.org/t/p/w500${apiResponse.results[a].poster_path}`,
+            apiResponse.results[a].release_date.slice(0,4),
+            apiResponse.results[a].original_title,
+            apiResponse.results[a].vote_average
+        )
+        movieBanner.appendChild(element)
     }
 }) ()
-
 
