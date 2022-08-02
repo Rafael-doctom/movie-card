109118b47de1c39a2ce17037a8d2354861e8cd53
Guilherme <guilherme.salviano@hotmail.com>
2022-08-02 19:51:35 -0300

    the code has been enhanced

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,73 @@
+//This function removel all childNodes of some element
 export function removeAllChildNodes (parent){
     while (parent.firstChild){
         parent.removeChild(parent.firstChild)
+    }
+}
+
+//This function makes a request to the TMDB API
+export function request (page = 1){
+    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=103186a9f4ef77e1f666cdd93a1fa70a&page=${page}&sort_by=popularity.desc`, {
+        method: 'GET',
+        headers:{
+            'Content-Type': 'application/json;charset=utf-8'
+        }
+    })
+    
+}
+
+///This const execute an arrow function that returns the component which is inserted in movieBanner element
+export const component = (id, src, year, name, likes) =>{
+    const content = ` 
+    <div class="movie__box" data-id="${id}">
+    <img src="${src}" alt="" class="movies__img" 
+    <div class="movie__details">
+        <p class="details__name">${name}</p>
+        <p class="details__year">Ano de lançamento: ${year}</p>
+        <p class="details__likes">Avaliação: ${likes}</p>
+    </div>
+    </div>`
+
+  const element = document.createElement('div')
+  element.classList.add('movie__container')
+  element.innerHTML = content
+  return element
+}
+
+//This function inserts the folders inside the movieBanner element
+export function mountMovieFolders(apiResponse, movieBanner){
+    //this looping works as many times as results we have in apiResponse
+    for(let a = 0; a < apiResponse.results.length; a++){
+        const element = component(
+            apiResponse.results[a].id,
+            `https://image.tmdb.org/t/p/w500${apiResponse.results[a].poster_path}`,
+            apiResponse.results[a].release_date.slice(0,4),
+            apiResponse.results[a].original_title,
+            apiResponse.results[a].vote_average
+        )
+        movieBanner.appendChild(element)
+    }
+}
+//This function organizes the moveButtons used to move between the pages of the API.
+export function organizeMoveButtons(direction, goButton){
+    const firstButton = document.querySelector('[data-button="1"]')
+    const secondButton = document.querySelector('[data-button="2"]')
+    const thirdButton = document.querySelector('[data-button="3"]')
+    const forthButton = document.querySelector('[data-button="4"]')
+
+    switch(direction){
+        case true:
+            firstButton.innerHTML = goButton + 1
+            secondButton.innerHTML = goButton + 2
+            thirdButton.innerHTML = goButton + 3
+            forthButton.innerHTML = goButton + 4
+            break
+        case false:
+            firstButton.innerHTML = goButton - 4
+            secondButton.innerHTML = goButton - 3 
+            thirdButton.innerHTML = goButton - 2 
+            forthButton.innerHTML = goButton - 1 
+            break
     }
 }
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,8 @@
-import { removeAllChildNodes } from "./functions.js"
-
-
+import { removeAllChildNodes, request, mountMovieFolders, organizeMoveButtons } from "./functions.js"
 
 // guilherme dará continuidade aqui
 
-//Component inserted inside the section "movies":
-
-const component = (id, src, year, name, likes) =>{
-    const content = ` 
-    <div class="movie__box" data-id="${id}">
-    <img src="${src}" alt="" class="movies__img" 
-    <div class="movie__details">
-        <p class="details__name">${name}</p>
-        <p class="details__year">Ano de lançamento: ${year}</p>
-        <p class="details__likes">Avaliação: ${likes}</p>
-    </div>
-    </div>`
-
-  const element = document.createElement('div')
-  element.classList.add('movie__container')
-  element.innerHTML = content
-  return element
-}
-
-//Here we make a request to tmdb api
-export function request (page = 1){
-    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=103186a9f4ef77e1f666cdd93a1fa70a&page=${page}&sort_by=popularity.desc`, {
-        method: 'GET',
-        headers:{
-            'Content-Type': 'application/json;charset=utf-8'
-        }
-    })
-    
-}
-//Here we show the folders
+//Here we show the folders as soon as the index page is open
 (async()=>{
     const api = await request()
     const apiResponse = await api.json()
@@ -41,103 +10,45 @@ export function request (page = 1){
     //This const get the section where we append each movie folder
     const movieBanner = document.querySelector('[data-movies]')
     //This looping add a new movie folder based on the api response. We'll have as many folders as loopings we do
-    for(let a = 0; a < 20; a++){
-        const element = component(
-            apiResponse.results[a].id,
-            `https://image.tmdb.org/t/p/w500${apiResponse.results[a].poster_path}`,
-            apiResponse.results[a].release_date.slice(0,4),
-            apiResponse.results[a].original_title,
-            apiResponse.results[a].vote_average
-        )
-        movieBanner.appendChild(element)
-    }
+    mountMovieFolders(apiResponse, movieBanner)
 }) ()
 
 const pageButtons = document.querySelector('[data-page]')
-
+//this is an addEventListener aplied on the buttons used to move between the API pages
 pageButtons.addEventListener('click', event => {
     let button = event.target
-    //this if works when we click on a number
+    const movieBanner = document.querySelector('[data-movies]')
+    //this if works just when we click on a number
     if(parseInt(button.innerHTML) > 0) {
         (async()=>{
-            const movieBanner = document.querySelector('[data-movies]')
             const newApiRequest = await request(parseInt(button.innerHTML))
             const newApiResponse = await newApiRequest.json()
-            console.log(newApiResponse)
             removeAllChildNodes(movieBanner)
-            for(let a = 0; a < 20; a++){
-                const element = component(
-                    newApiResponse.results[a].id,
-                    `https://image.tmdb.org/t/p/w500${newApiResponse.results[a].poster_path}`,
-                    newApiResponse.results[a].release_date.slice(0,4),
-                    newApiResponse.results[a].original_title,
-                    newApiResponse.results[a].vote_average
-                )
-                movieBanner.appendChild(element)
-            }
-            
+            mountMovieFolders(newApiResponse, movieBanner)
         })()
     } 
-    //this if works when we click on the last button, with ">>" signal
-    if(button.dataset.button == 'right') {
-        (async()=>{
-            const goButton = parseInt(document.querySelector('[data-button="4"]').innerHTML)
-            const movieBanner = document.querySelector('[data-movies]')
-            const newApiRequest = await request(goButton + 1)
-            const newApiResponse = await newApiRequest.json()
-            console.log(newApiResponse)
-            removeAllChildNodes(movieBanner)
-            for(let a = 0; a < 20; a++){
-                const element = component(
-                    newApiResponse.results[a].id,
-                    `https://image.tmdb.org/t/p/w500${newApiResponse.results[a].poster_path}`,
-                    newApiResponse.results[a].release_date.slice(0,4),
-                    newApiResponse.results[a].original_title,
-                    newApiResponse.results[a].vote_average
-                )
-                movieBanner.appendChild(element)
-            }
-            const firstButton = document.querySelector('[data-button="1"]')
-            const secondButton = document.querySelector('[data-button="2"]')
-            const thirdButton = document.querySelector('[data-button="3"]')
-            const forthButton = document.querySelector('[data-button="4"]')
-
-            firstButton.innerHTML = goButton + 1
-            secondButton.innerHTML = goButton + 2
-            thirdButton.innerHTML = goButton + 3
-            forthButton.innerHTML = goButton + 4
-        })()
-    } 
-    //this if works when we click on the first button, with "<<" signal
-    if(button.dataset.button == 'left'){
-        (async()=>{
-            const backButton = parseInt(document.querySelector('[data-button="1"]').innerHTML)
-            const movieBanner = document.querySelector('[data-movies]')
-            if(backButton == 1) return
-            const newApiRequest = await request(backButton - 4)
-            const newApiResponse = await newApiRequest.json()
-            console.log(newApiResponse)
-            removeAllChildNodes(movieBanner)
-            for(let a = 0; a < 20; a++){
-                const element = component(
-                    newApiResponse.results[a].id,
-                    `https://image.tmdb.org/t/p/w500${newApiResponse.results[a].poster_path}`,
-                    newApiResponse.results[a].release_date.slice(0,4),
-                    newApiResponse.results[a].original_title,
-                    newApiResponse.results[a].vote_average
-                )
-                movieBanner.appendChild(element)
-            }
-            const firstButton = document.querySelector('[data-button="1"]')
-            const secondButton = document.querySelector('[data-button="2"]')
-            const thirdButton = document.querySelector('[data-button="3"]')
-            const forthButton = document.querySelector('[data-button="4"]')
-
-            firstButton.innerHTML = backButton - 4
-            secondButton.innerHTML = backButton - 3 
-            thirdButton.innerHTML = backButton - 2 
-            forthButton.innerHTML = backButton - 1 
-        })()
+    //this switch works when the button clicked is right ">>" or left "<<"
+    switch(button.dataset.button){
+        case 'right':
+            (async()=>{
+                const goButton = parseInt(document.querySelector('[data-button="4"]').innerHTML)
+                const newApiRequest = await request(goButton + 1)
+                const newApiResponse = await newApiRequest.json()
+                removeAllChildNodes(movieBanner)
+                mountMovieFolders(newApiResponse, movieBanner)
+                organizeMoveButtons(true, goButton)
+            })()
+            break
+        case 'left':
+            (async()=>{
+                const backButton = parseInt(document.querySelector('[data-button="1"]').innerHTML)
+                if(backButton == 1) return
+                const newApiRequest = await request(backButton - 4)
+                const newApiResponse = await newApiRequest.json()
+                removeAllChildNodes(movieBanner)
+                mountMovieFolders(newApiResponse, movieBanner)
+                organizeMoveButtons(false, backButton)
+            })() 
     }
 })
 
